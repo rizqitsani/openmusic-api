488dd7696b442185304995616ef9ce11cee30ee0
rizqitsani <rizqitsani@gmail.com>
2022-03-23 13:10:48 +0700

    feat: add user crud

--- src/server.js
+++ src/server.js
@@ -4,15 +4,19 @@ const Hapi = require('@hapi/hapi');
 
 const album = require('./api/album');
 const song = require('./api/song');
+const user = require('./api/user');
 const ClientError = require('./exceptions/ClientError');
 const AlbumService = require('./services/postgres/AlbumService');
 const SongService = require('./services/postgres/SongService');
+const UserService = require('./services/postgres/UserService');
 const AlbumValidator = require('./validator/album');
 const SongValidator = require('./validator/song');
+const UserValidator = require('./validator/user');
 
 const init = async () => {
   const albumService = new AlbumService();
   const songService = new SongService();
+  const userService = new UserService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -39,21 +43,29 @@ const init = async () => {
     return response.continue || response;
   });
 
-  await server.register({
-    plugin: album,
-    options: {
-      service: albumService,
-      validator: AlbumValidator,
+  await server.register([
+    {
+      plugin: album,
+      options: {
+        service: albumService,
+        validator: AlbumValidator,
+      },
     },
-  });
-
-  await server.register({
-    plugin: song,
-    options: {
-      service: songService,
-      validator: SongValidator,
+    {
+      plugin: song,
+      options: {
+        service: songService,
+        validator: SongValidator,
+      },
     },
-  });
+    {
+      plugin: user,
+      options: {
+        service: userService,
+        validator: UserValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
